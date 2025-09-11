65910d1aacf8c7d9adb667f97c019487eeebedfa
Alexey Chizhov <alexchizhov90@gmail.com>
2025-09-11 04:26:01 +0300

    Fix

--- grpc_course_client.py
+++ grpc_course_client.py
@@ -2,6 +2,7 @@ import grpc
 import course_service_pb2
 import course_service_pb2_grpc
 
+
 def run():
     # Устанавливаем соединение с сервером
     channel = grpc.insecure_channel('localhost:50051')
